8ee1183939f8555ad0ba26298a48d89f0b8f13b9
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-03-27 11:08:40 +0530

    Product array

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import SingleProduct from './SingleProduct';
+import './style.css';
+import faker from 'faker';
 
 const Home = () => {
+  const productArray = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+  }));
+
   return (
     <div>
       <h1>This is Home.js</h1>
-      <SingleProduct/>
+      <SingleProduct />
     </div>
   );
 };
