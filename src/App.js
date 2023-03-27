b3995e6f2d59ff1099d5c09d4017ccf28578ee7f
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-03-27 10:34:27 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+
 const App = () => {
-  return <>App.js</>;
+  return (
+    <div>
+      <h1>This is App.js </h1>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
