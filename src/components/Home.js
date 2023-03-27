912566da202e0c81aa3fa6d7a03188ace4f1812f
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-03-27 11:00:32 +0530

    singleproduct

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SingleProduct from './SingleProduct';
 
 const Home = () => {
   return (
     <div>
       <h1>This is Home.js</h1>
+      <SingleProduct/>
     </div>
   );
 };
